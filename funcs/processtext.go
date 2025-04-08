b06279c296a929878c5ec053df27fabc98742d1d
Ardak Tleules <artleules@noreply.01.tomorrow-school.ai>
2025-04-08 12:32:20 +0000

    Add funcs/processtext.go

---
+++ funcs/processtext.go
@@ -0,0 +1,26 @@
+package firstproject
+
+import (
+	"strings"
+)
+
+func ProcessText(text string) string {
+
+	lines := strings.Split(text, "\n")
+
+	for i, line := range lines {
+		line = Punctuation(line) // обработка пунктуации
+		words := strings.Fields(line)
+		words = MergeStrings(words)     // merge "(cap," + "<number>)", также отделяет от остальных
+		words = ProcessCommands(words)  //(up), (cap), (low) functions then articles
+		words = CommandsWithNums(words) // (cap|up|low, <numbers>)
+		words = HexBinProcess(words)    //binhex commands
+
+		lines[i] = strings.Join(words, " ")
+		lines[i] = Punctuation(lines[i]) //контрольная коррекция пунктуации после команд
+	}
+
+	result := strings.Join(lines, "\n")
+
+	return result
+}
